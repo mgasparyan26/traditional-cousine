df0336b10a1991d957640e545a5c80061c31b547
Babken Vardanyan <babken@lazydevelo.com>
2017-12-16 11:34:12 +0400

    Added models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from core import models
+
+admin.register(models.Ingredient)
+admin.register(models.Location)
+admin.register(models.Recipe)
